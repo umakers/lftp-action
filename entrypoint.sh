7211c1be5650afa68c5ba65a36e4d1b717987e04
Jesper Ordrup <jesper@umakers.dk>
2020-05-07 21:15:53 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,3 @@
 #!/bin/sh -l
 
-lftp -c "set ftp:ssl-allow no; open -u $INPUT_USER,$INPUT_PASSWORD $INPUT_HOST mirror -Rev . ./public_html  --ignore-time --parallel=10 --exclude-glob .git* --exclude .git/ --exclude App_Data/ --exclude ^\media/$ --exclude ^\Media/$"
+lftp -c "set ftp:ssl-allow no; open -u $INPUT_USER,$INPUT_PASSWORD $INPUT_HOST mirror -R . ./public_html  --ignore-time --parallel=10 --exclude-glob .git* --exclude .git/ --exclude App_Data/ --exclude ^\media/$ --exclude ^\Media/$"
